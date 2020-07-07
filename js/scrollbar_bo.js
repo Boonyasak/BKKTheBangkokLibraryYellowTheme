58c12c328ada6df4891c331955552361eb0a480c
akradech <artmodern.2562@gmail.com>
2020-07-07 18:41:25 +0700

    yellow

---
+++ js/scrollbar_bo.js
@@ -0,0 +1,16 @@
+$(document).scroll(function() {
+   if(hasTransparent){
+        if( $(this).scrollTop() > 260 ) {
+            if(transparent) {
+                transparent = false;
+                $('nav[role="navigation"]').removeClass('navbar-transparent');
+            }
+        } else {
+            if( !transparent ) {
+                transparent = true;
+                $('nav[role="navigation"]').addClass('navbar-transparent');
+            }
+        }
+    }
+});
+
